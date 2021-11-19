8eacec15a2147e8b39329b219f805e2403fc2378
MonAmiral <jerome.bretheau@gmail.com>
2021-11-19 15:38:47 +0100

    Ajout du projet Unity

---
+++ JamWithUsUnityProject/Assets/Scripts/Settings/Settings.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Settings
+{
+	private static bool initialized;
+
+	public static System.Action<float> SFXVolumeChanged;
+	public static System.Action<float> MusicVolumeChanged;
+
+	private static float sfxVolume = 1f;
+	public static float SFXVolume
+	{
+		get
+		{
+			if (!initialized)
+			{
+				Initialize();
+			}
+
+			return sfxVolume;
+		}
+
+		set
+		{
+			sfxVolume = value;
+			PlayerPrefs.SetFloat("HoloWEEN_SFXVolume", value);
+			PlayerPrefs.Save();
+
+			SFXVolumeChanged?.Invoke(value);
+		}
+	}
+
+	private static float musicVolume = 1f;
+	public static float MusicVolume
+	{
+		get
+		{
+			if (!initialized)
+			{
+				Initialize();
+			}
+
+			return musicVolume;
+		}
+
+		set
+		{
+			musicVolume = value;
+			PlayerPrefs.SetFloat("HoloWEEN_MusicVolume", value);
+			PlayerPrefs.Save();
+
+			MusicVolumeChanged?.Invoke(value);
+		}
+	}
+
+	private static void Initialize()
+	{
+		initialized = true;
+
+		sfxVolume = PlayerPrefs.GetFloat("HoloWEEN_SFXVolume", 1f);
+		musicVolume = PlayerPrefs.GetFloat("HoloWEEN_MusicVolume", 1f);
+	}
+}
---
+++ JamWithUsUnityProject/Assets/Scripts/UI/SettingsMenu.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+	public Slider MusicSlider, SFXSlider;
+
+	public TMPro.TMP_InputField WidthField, HeightField;
+	public Toggle FullScreenToggle;
+
+	public Animator Curtain;
+	private string pendingSceneName;
+
+	private void OnEnable()
+	{
+		this.WidthField.text = Screen.width.ToString();
+		this.HeightField.text = Screen.height.ToString();
+		this.FullScreenToggle.SetIsOnWithoutNotify(Screen.fullScreen);
+
+		this.SFXSlider.SetValueWithoutNotify(Settings.SFXVolume);
+		this.MusicSlider.SetValueWithoutNotify(Settings.MusicVolume);
+
+		Time.timeScale = 0f;
+	}
+
+	private void OnDisable()
+	{
+		Time.timeScale = 1f;
+	}
+
+	public void SetMusicVolume(float value)
+	{
+		Settings.MusicVolume = value;
+	}
+
+	public void SetSFXVolume(float value)
+	{
+		Settings.SFXVolume = value;
+	}
+
+	public void ToggleFullScreen(bool newValue)
+	{
+		Screen.fullScreen = newValue;
+	}
+
+	public void ApplyResolution()
+	{
+		Screen.SetResolution(int.Parse(this.WidthField.text), int.Parse(this.HeightField.text), Screen.fullScreen);
+	}
+
+	public void LoadScene(string sceneName)
+	{
+		this.Curtain.Play("FadeIn");
+
+		this.pendingSceneName = sceneName;
+		this.Invoke(nameof(LoadPendingScene), 0.5f);
+		Time.timeScale = 1f;
+	}
+
+	private void LoadPendingScene()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene(this.pendingSceneName);
+	}
+}
